63839f3b16f38191f3caae25137b79187b44ee50
Nattanon <title409@icloud.com>
2017-08-03 01:43:36 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,10 +3,49 @@
 <body>
 
 <h1>Test</h1>
+  
+  
+  
+<?php
+
+/*  Insert SQL database location */
+$host = "localhost";
+/* Insert database user */
+$user = "robot";
+/* Insert database password */
+$pass = "Sudarat";
+/* Insert database name */
+$db = "robot_boat";
+mysql_connect($host, $user, $pass) or die("Could not connect to database");
+mysql_select_db($db) or die("Could not connect to database");
+mysql_query("SET NAMES utf8");
+
+$ddate = date('Y-m-d h:i:s');
+$pH = ($_POST['pH']);
+$DO = ($_POST['DO']);
+$Tur = ($_POST['pH']);
+$latitude = ($_POST['DO']);
+$longitude = ($_POST['pH']);
+$xl= ($_POST['pH']);
+$yl = ($_POST['DO']);
+
+$sql = "INSERT INTO boat VALUES ( $pH , $DO, $Tur,$latitude,$longitude,$xl,$yl,'$ddate' )";
+<<<<<<< HEAD:index.php
+$result=mysql_query($sql);
+
+=======
+$result=mysql_query($sql);
+>>>>>>> master:data.php
+
+?>
 
 <?php
 echo "Hello World!";
+echo $pH; 
 ?>
+  
+  
+  
 
 </body>
 </html>
