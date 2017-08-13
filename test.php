a54224cdedc6ce9a50ee150a90e412dd95cec6e1
Nattanon Dungsunenarn <title409@icloud.com>
2017-08-14 01:45:10 +0700

    test

--- test.php
+++ test.php
@@ -1,18 +1,29 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>Test</h1>
-  
-  
-  
-
 <?php
-echo "Hello World!";
-?>
-  
-  
-  
 
-</body>
-</html>
+$host = "localhost";
+$user = "robot";
+$pass = "Sudarat";
+$db = "robot_boat";
+mysql_connect($host, $user, $pass) or die("Could not connect to database");
+mysql_select_db($db) or die("Could not connect to database");
+mysql_query("SET NAMES utf8");
+
+$ddate = date('Y-m-d h:i:s');
+$pH = ($_POST['pH']);
+$DO = ($_POST['DO']);
+$Tur = ($_POST['pH']);
+$latitude = ($_POST['DO']);
+$longitude = ($_POST['pH']);
+$xl= ($_POST['pH']);
+$yl = ($_POST['DO']);
+
+
+
+$sql = "INSERT INTO boat VALUES ( $pH , $DO, $Tur,$latitude,$longitude,$xl,$yl,'$ddate' )";
+$result=mysql_query($sql);
+
+
+echo" $pH";
+
+
+?>
